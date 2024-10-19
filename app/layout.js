8580260b2c0b5c4e83903b19aec40c2985f553f3
My Name <thesame@email.com>
2024-10-20 05:16:48 +0800

    starting changes

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,12 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
 import CreateEventDrawer from "@/components/create-event";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Schedulrr",
+  title: "KMCPortal",
   description: " ",
 };
 
@@ -16,16 +17,23 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Header />
-          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-            {children}
-          </main>
-          <footer className="bg-blue-100 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
-            </div>
-          </footer>
-          <CreateEventDrawer />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+              {children}
+            </main>
+            <footer className="bg-blue-100 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-600">
+                <p>Gold Buster</p>
+              </div>
+            </footer>
+            <CreateEventDrawer />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
